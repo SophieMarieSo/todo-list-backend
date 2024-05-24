d2b0a58b8b19879cf4e6c9a2f63f4788c8640f2e
AhnEunJin <modic970515@gmail.com>
2024-05-25 00:41:32 +0900

    cors 에러 해결 npm 설치

--- todo-list-be/app.js
+++ todo-list-be/app.js
@@ -2,18 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', indexRouter);
+app.use(cors());
 
 const mongoURI = `mongodb://localhost:27017/todo-list`;
 
 mongoose
-    .connect(mongoURI, { useNewUrlParser: true })
-    .then(() => console.log('mongoose connected'))
-    .catch((err) => console.log('DB connection failed', err));
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('mongoose connected'))
+  .catch((err) => console.log('DB connection failed', err));
 
 app.listen(5000, () => {
-    console.log('server is on 5000');
+  console.log('server is on 5000');
 });
