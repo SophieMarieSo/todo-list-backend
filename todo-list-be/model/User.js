d1a02e1440fe0cbaec97a41ef8994ba91f24b156
AhnEunJin <modic970515@gmail.com>
2024-05-27 01:33:29 +0900

    로그인 be

--- todo-list-be/model/User.js
+++ todo-list-be/model/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Schema = mongoose.Schema;
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema(
   {
@@ -18,6 +21,18 @@ const userSchema = Schema(
   },
   { timestamps: true }
 );
+
+// 항상 password를 빼기 위해 ...
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this.id }, JWT_SECRET_KEY, { expiresIn: '1d' });
+  return token;
+};
 
 const User = mongoose.model('User', userSchema);
 
--- todo-list-be/routes/user.api.js
+++ todo-list-be/routes/user.api.js
@@ -3,5 +3,7 @@ const userController = require('../controller/user.controller');
 const router = express.Router();
 
 router.post('/', userController.createUser);
+// get은 reqBody를 사용할 수 없다
+router.post('/login', userController.loginWithEmail);
 
 module.exports = router;
