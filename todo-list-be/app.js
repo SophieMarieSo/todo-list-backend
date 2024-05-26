64942ff5fdf9f469933bd68e222456730aa26a18
AhnEunJin <modic970515@gmail.com>
2024-05-27 05:56:15 +0900

    MongoDb Altas 배포

--- todo-list-be/app.js
+++ todo-list-be/app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
+require('dotenv').config();
 const cors = require('cors');
 
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 const app = express();
+console.log('====================================');
+console.log(MONGODB_URI_PROD);
+console.log('====================================');
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-list`;
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
